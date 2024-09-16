1e2f031d78eb8f8a63908bf92b7b77a5290f2e1c
sardonicus87 <fx_forever@mail.com>
2024-09-15 20:00:38 -0500

    Added "double tap to exit" fuctionality.

--- app/src/main/java/com/example/tobaccocellar/MainActivity.kt
+++ app/src/main/java/com/example/tobaccocellar/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tobaccocellar
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +18,31 @@ import com.example.tobaccocellar.data.LocalCellarApplication
 import com.example.tobaccocellar.ui.theme.TobaccoCellarTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var backPressedOnce = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object :
+            OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (backPressedOnce) {
+                    finish()
+                    return
+                }
+
+                backPressedOnce = true
+                Toast.makeText(this@MainActivity, "Press BACK again to exit", Toast.LENGTH_SHORT)
+                    .show()
+
+                Handler(Looper.getMainLooper()).postDelayed({ backPressedOnce = false }, 2000)
+            }
+        })
+
 //        enableEdgeToEdge()
 //        WindowCompat.setDecorFitsSystemWindows(window, true)
+
 
         setContent {
             val application = (application as CellarApplication)
