e22550eeefcb73660d756e357484c10d1e5f6e90
sardonicus87 <fx_forever@mail.com>
2024-11-13 01:18:13 -0600

    Cleanup

--- app/src/main/java/com/example/tobaccocellar/MainActivity.kt
+++ app/src/main/java/com/example/tobaccocellar/MainActivity.kt
@@ -52,8 +52,6 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize(),
                         color = MaterialTheme.colorScheme.background,
-                    //    tonalElevation = 1.dp
-
                     ) {
                         CellarApp()
                     }
