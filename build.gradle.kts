b542c01329b64ed7418f8feadedb27d517ee9644
sardonicus87 <fx_forever@mail.com>
2024-11-13 20:17:18 -0600

    Update kotlin dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("com.android.library") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.10" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
 
