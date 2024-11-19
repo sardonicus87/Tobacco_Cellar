584320bf93a57eb2e3606bbd1a2e98a066cb2b1e
sardonicus87 <fx_forever@mail.com>
2024-11-19 03:44:45 -0600

    LaunchedEffect to ensure save validation runs when first loaded in the rare event of an item able to be entered with blank brand or blend field

--- app/src/main/java/com/example/tobaccocellar/ui/items/EditEntryScreen.kt
+++ app/src/main/java/com/example/tobaccocellar/ui/items/EditEntryScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -47,6 +48,10 @@ fun EditEntryScreen(
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     val coroutineScope = rememberCoroutineScope()
     val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(key1 = viewModel.itemUiState) {
+        viewModel.updateUiState(viewModel.itemUiState.itemDetails)
+    }
 
     fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
         this.clickable(
