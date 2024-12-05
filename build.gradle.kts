474605db1fe34190e899f45264005729831a7471
sardonicus87 <fx_forever@mail.com>
2024-12-05 01:44:59 -0600

    Dependency update and move android library plugin to TOML

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("room_version", "2.6.0")
+        set("room_version", "2.6.1")
     }
 }
 
 plugins {
-    id("com.android.library") version "8.7.2" apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.devtools.ksp) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
