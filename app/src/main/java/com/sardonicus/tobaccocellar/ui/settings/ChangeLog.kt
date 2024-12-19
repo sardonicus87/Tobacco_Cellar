5827f381e160c4e9abd05c382f033167de08ca9d
sardonicus87 <fx_forever@mail.com>
2024-12-19 17:35:16 -0600

    Change order of logs (newest at top) for easier updating, updated some logs.

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/ChangeLog.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/ChangeLog.kt
@@ -2,10 +2,24 @@ package com.sardonicus.tobaccocellar.ui.settings
 
 
 val changeLogEntries = listOf(
+
+//    ChangeLogEntryData(
+//        versionNumber = "",
+//        buildDate = "",
+//        changes = listOf(),
+//        improvements = listOf(),
+//        bugFixes = emptyList(),
+//    ),
+
     ChangeLogEntryData(
-        versionNumber = "1.0.0",
-        buildDate = "20 Nov, 2024",
-        changes = listOf("Initial Release"),
+        versionNumber = "1.3.0",
+        buildDate = "13 Dec, 2024",
+        changes = listOf(
+            "Renamed \"Quantity\" to \"Tins\". Tins is now the baseline quantity unit.",
+            "Added a converter in add/edit screens for setting the quantity of tins by " +
+                    "ounces, pounds, or grams.",
+            "Added a setting to the Settings screen for changing the tin conversion rate for " +
+                    "ounces and grams (separate conversion rates)."),
         improvements = emptyList(),
         bugFixes = emptyList(),
     ),
@@ -19,71 +33,25 @@ val changeLogEntries = listOf(
             "Added Change Log to settings screen."
         ),
         improvements = listOf(
-            "List and Table views auto scroll to top on filter and sort changes, scroll to new item on insert.",
+            "List and Table views auto scroll to top on filter and sort changes, scroll to " +
+                    "new item on insert.",
             "Other minor UI improvements."
         ),
         bugFixes = listOf(
-            "Fixed minor bug where newly added brands weren't available for filtering until app restart.",
-            "Fixed bug where app would crash when trying to save an edit with a non-unique combination of Brand and Blend."
+            "Fixed minor bug where newly added brands weren't available for filtering until app " +
+                    "restart.",
+            "Fixed bug where app would crash when trying to save an edit with a non-unique " +
+                    "combination of Brand and Blend."
         ),
     ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
-//    ChangeLogEntryData(
-//        versionNumber = "",
-//        buildDate = "",
-//        changes = listOf(),
-//        improvements = listOf(),
-//        bugFixes = emptyList(),
-//    ),
+    ChangeLogEntryData(
+        versionNumber = "1.0.0",
+        buildDate = "20 Nov, 2024",
+        changes = listOf("Initial Release"),
+        improvements = emptyList(),
+        bugFixes = emptyList(),
     )
+)
 
 
 
