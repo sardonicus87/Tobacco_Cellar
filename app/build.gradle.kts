64c570305e403c1ce1cec1c02192719dfe5cfe2b
sardonicus87 <fx_forever@mail.com>
2024-12-25 02:53:04 -0600

    Increase versionCode to 4 in anticipation of the next build, added ndk debugSymbolLevel to release build.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.sardonicus.tobaccocellar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.3.1"
 
         vectorDrawables {
@@ -31,6 +31,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
     }
     compileOptions {
