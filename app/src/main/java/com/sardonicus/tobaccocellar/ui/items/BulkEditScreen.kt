b8131ae2f6b4ccaedf5f47eba5ed6040f32215c3
sardonicus87 <fx_forever@mail.com>
2025-02-18 02:23:12 -0600

    Optimize imports

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/items/BulkEditScreen.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/items/BulkEditScreen.kt
@@ -1,6 +1,5 @@
 package com.sardonicus.tobaccocellar.ui.items
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
