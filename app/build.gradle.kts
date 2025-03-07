2391e32b88d01ce724b244d51061b68fa961eac5
sardonicus87 <fx_forever@mail.com>
2025-03-07 03:09:01 -0600

    Update version code and name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sardonicus.tobaccocellar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.4.0"
+        versionCode = 11
+        versionName = "2.5.0"
 
         vectorDrawables {
             useSupportLibrary = true
