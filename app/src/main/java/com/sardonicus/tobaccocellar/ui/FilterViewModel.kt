9675ddc9cb2aa767127853ec64e36c82e64c459b
sardonicus87 <fx_forever@mail.com>
2025-04-21 01:02:22 -0500

    Moved filtering val declarations to top of view model (just in case, maybe prevent potential future out-of-bounds/null error).

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/FilterViewModel.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/FilterViewModel.kt
@@ -105,6 +105,63 @@ class FilterViewModel (
     val sheetSelectedOutOfProduction = MutableStateFlow(false)
 
 
+    // inclusionary filter states //
+    private val _selectedBrands = MutableStateFlow<List<String>>(emptyList())
+    val selectedBrands: StateFlow<List<String>> = _selectedBrands
+
+    private val _selectedTypes = MutableStateFlow<List<String>>(emptyList())
+    val selectedTypes: StateFlow<List<String>> = _selectedTypes
+
+    private val _selectedUnassigned = MutableStateFlow(false)
+    val selectedUnassigned: StateFlow<Boolean> = _selectedUnassigned
+
+    private val _selectedFavorites = MutableStateFlow(false)
+    val selectedFavorites: StateFlow<Boolean> = _selectedFavorites
+
+    private val _selectedDislikeds = MutableStateFlow(false)
+    val selectedDislikeds: StateFlow<Boolean> = _selectedDislikeds
+
+    private val _selectedNeutral = MutableStateFlow(false)
+    val selectedNeutral: StateFlow<Boolean> = _selectedNeutral
+
+    private val _selectedNonNeutral = MutableStateFlow(false)
+    val selectedNonNeutral: StateFlow<Boolean> = _selectedNonNeutral
+
+    private val _selectedInStock = MutableStateFlow(false)
+    val selectedInStock: StateFlow<Boolean> = _selectedInStock
+
+    private val _selectedOutOfStock = MutableStateFlow(false)
+    val selectedOutOfStock: StateFlow<Boolean> = _selectedOutOfStock
+
+    private val _selectedSubgenre = MutableStateFlow<List<String>>(emptyList())
+    val selectedSubgenre: StateFlow<List<String>> = _selectedSubgenre
+
+    private val _selectedCut = MutableStateFlow<List<String>>(emptyList())
+    val selectedCut: StateFlow<List<String>> = _selectedCut
+
+    private val _selectedComponent = MutableStateFlow<List<String>>(emptyList())
+    val selectedComponent: StateFlow<List<String>> = _selectedComponent
+
+    private val _compMatching = MutableStateFlow<String>("Any")
+    val compMatching: StateFlow<String> = _compMatching
+
+    private val _selectedProduction = MutableStateFlow(false)
+    val selectedProduction: StateFlow<Boolean> = _selectedProduction
+
+    private val _selectedOutOfProduction = MutableStateFlow(false)
+    val selectedOutOfProduction: StateFlow<Boolean> = _selectedOutOfProduction
+
+    // exclusionary filter states //
+    private val _selectedExcludeBrands = MutableStateFlow<List<String>>(emptyList())
+    val selectedExcludeBrands: StateFlow<List<String>> = _selectedExcludeBrands
+
+    private val _selectedExcludeLikes = MutableStateFlow(false)
+    val selectedExcludeLikes: StateFlow<Boolean> = _selectedExcludeLikes
+
+    private val _selectedExcludeDislikes = MutableStateFlow(false)
+    val selectedExcludeDislikes: StateFlow<Boolean> = _selectedExcludeDislikes
+
+
     // filter applied state for clear all button //
     val isFilterApplied: StateFlow<Boolean> = combine(
         sheetSelectedBrands,
@@ -316,62 +373,6 @@ class FilterViewModel (
         }
     }
 
-
-    // inclusionary filter states //
-    private val _selectedBrands = MutableStateFlow<List<String>>(emptyList())
-    val selectedBrands: StateFlow<List<String>> = _selectedBrands
-
-    private val _selectedTypes = MutableStateFlow<List<String>>(emptyList())
-    val selectedTypes: StateFlow<List<String>> = _selectedTypes
-
-    private val _selectedUnassigned = MutableStateFlow(false)
-    val selectedUnassigned: StateFlow<Boolean> = _selectedUnassigned
-
-    private val _selectedFavorites = MutableStateFlow(false)
-    val selectedFavorites: StateFlow<Boolean> = _selectedFavorites
-
-    private val _selectedDislikeds = MutableStateFlow(false)
-    val selectedDislikeds: StateFlow<Boolean> = _selectedDislikeds
-
-    private val _selectedNeutral = MutableStateFlow(false)
-    val selectedNeutral: StateFlow<Boolean> = _selectedNeutral
-
-    private val _selectedNonNeutral = MutableStateFlow(false)
-    val selectedNonNeutral: StateFlow<Boolean> = _selectedNonNeutral
-
-    private val _selectedInStock = MutableStateFlow(false)
-    val selectedInStock: StateFlow<Boolean> = _selectedInStock
-
-    private val _selectedOutOfStock = MutableStateFlow(false)
-    val selectedOutOfStock: StateFlow<Boolean> = _selectedOutOfStock
-
-    private val _selectedSubgenre = MutableStateFlow<List<String>>(emptyList())
-    val selectedSubgenre: StateFlow<List<String>> = _selectedSubgenre
-
-    private val _selectedCut = MutableStateFlow<List<String>>(emptyList())
-    val selectedCut: StateFlow<List<String>> = _selectedCut
-
-    private val _selectedComponent = MutableStateFlow<List<String>>(emptyList())
-    val selectedComponent: StateFlow<List<String>> = _selectedComponent
-
-    private val _compMatching = MutableStateFlow<String>("Any")
-    val compMatching: StateFlow<String> = _compMatching
-
-    private val _selectedProduction = MutableStateFlow(false)
-    val selectedProduction: StateFlow<Boolean> = _selectedProduction
-
-    private val _selectedOutOfProduction = MutableStateFlow(false)
-    val selectedOutOfProduction: StateFlow<Boolean> = _selectedOutOfProduction
-
-    // exclusionary filter states //
-    private val _selectedExcludeBrands = MutableStateFlow<List<String>>(emptyList())
-    val selectedExcludeBrands: StateFlow<List<String>> = _selectedExcludeBrands
-
-    private val _selectedExcludeLikes = MutableStateFlow(false)
-    val selectedExcludeLikes: StateFlow<Boolean> = _selectedExcludeLikes
-
-    private val _selectedExcludeDislikes = MutableStateFlow(false)
-    val selectedExcludeDislikes: StateFlow<Boolean> = _selectedExcludeDislikes
 
     // remove selected filters if the last item with that filter is removed or changed //
     init {
