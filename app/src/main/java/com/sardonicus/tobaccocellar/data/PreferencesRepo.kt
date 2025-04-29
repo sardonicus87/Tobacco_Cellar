38726671047e3f28cb8760d542658f69e38a0613
sardonicus87 <fx_forever@mail.com>
2025-04-29 02:39:36 -0500

    Removed flavoring from search settings (added to filtering)

--- app/src/main/java/com/sardonicus/tobaccocellar/data/PreferencesRepo.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/data/PreferencesRepo.kt
@@ -216,7 +216,6 @@ class PreferencesRepo(
                 SearchSetting.BLEND.value -> SearchSetting.BLEND
                 SearchSetting.NOTES.value -> SearchSetting.NOTES
                 SearchSetting.CONTAINER.value -> SearchSetting.CONTAINER
-                SearchSetting.FLAVOR.value -> SearchSetting.FLAVOR
                 else -> SearchSetting.BLEND
             }
         }
