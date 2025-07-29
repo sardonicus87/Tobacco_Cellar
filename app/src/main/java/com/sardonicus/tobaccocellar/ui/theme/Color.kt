dd88eed6871ceb9508f990b192cdef034a38a6a6
sardonicus87 <fx_forever@mail.com>
2025-07-29 18:38:37 -0500

    Set listMenuScrim alpha directly

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/theme/Color.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/theme/Color.kt
@@ -77,7 +77,7 @@ val surfaceContainerHighDark = Color(0xFF252B28)
 val surfaceContainerHighestDark = Color(0xFF303632)
 
 // custom colors //
-val listMenuScrimLight = Color(0xFFFFFFFF)
+val listMenuScrimLight = Color(0xBFFFFFFF)
 val deleteButtonLight = Color(0xFF93000A)
 val favHeartLight = Color(0xFFBB0000)
 val disHeartLight = Color(0xFF03B1FF)
@@ -94,7 +94,7 @@ val sheetBoxLight = Color(0xFFE6F0E6)
 val sheetBoxBorderLight = Color(0xFFB5BDB5)
 val indicatorBorderCorrectionLight = Color(0xFF59AD86)
 
-val listMenuScrimDark = Color(0xFF000000)
+val listMenuScrimDark = Color(0xBF000000)
 val deleteButtonDark = Color(0xFF990000)
 val favHeartDark = Color(0xFFCF0000)
 val disHeartDark = Color(0xFF03B1FF)
