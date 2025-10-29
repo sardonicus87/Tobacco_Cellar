cebbbc27041852bf878a1445e51c019e2a9f2787
sardonicus87 <fx_forever@mail.com>
2025-10-29 01:01:08 -0500

    Functions updated to take ratings settings and items data (data fed directly to functions instead of from a database call in the override functions, change for allowing all items or currently filtered items and remove unnecessary database call)

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/utilities/ExportCsvHandler.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/utilities/ExportCsvHandler.kt
@@ -1,8 +1,9 @@
 package com.sardonicus.tobaccocellar.ui.utilities
 
 import android.net.Uri
+import com.sardonicus.tobaccocellar.ui.settings.ExportRating
 
 interface ExportCsvHandler {
-    fun onExportCsvClick(uri: Uri?)
-    fun onTinsExportCsvClick(uri: Uri?)
+    fun onExportCsvClick(uri: Uri?, allItems: Boolean, exportRating: ExportRating)
+    fun onTinsExportCsvClick(uri: Uri?, allItems: Boolean, exportRating: ExportRating)
 }
