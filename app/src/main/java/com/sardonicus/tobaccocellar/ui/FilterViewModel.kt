8f9d1fcf9d9116fb8df61f060e293da09118b720
sardonicus87 <fx_forever@mail.com>
2025-12-03 17:39:03 -0600

    .

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/FilterViewModel.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/FilterViewModel.kt
@@ -1,6 +1,5 @@
 package com.sardonicus.tobaccocellar.ui
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sardonicus.tobaccocellar.data.Components
