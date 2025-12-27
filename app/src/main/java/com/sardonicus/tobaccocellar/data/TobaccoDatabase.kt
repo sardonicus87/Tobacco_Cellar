516ab91cec7dadeba6b60089fb8a36a19b5baf90
sardonicus87 <fx_forever@mail.com>
2025-12-27 04:20:55 -0600

    .

--- app/src/main/java/com/sardonicus/tobaccocellar/data/TobaccoDatabase.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/data/TobaccoDatabase.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.sardonicus.tobaccocellar.data.multiDeviceSync.PendingSyncOperation
+import com.sardonicus.tobaccocellar.data.multiDeviceSync.PendingSyncOperationDao
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(db: SupportSQLiteDatabase) {
