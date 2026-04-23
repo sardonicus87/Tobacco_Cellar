cb3517b8e28b9be7ff32135ea6c066951692360f
sardonicus87 <fx_forever@mail.com>
2026-04-23 03:20:15 -0500

    Ensure focus is cleared on RatingPopup disposal.

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/composables/RatingPopup.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/composables/RatingPopup.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -34,6 +35,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -58,6 +60,11 @@ fun RatingPopup(
 ) {
     var currentRatingString by rememberSaveable { mutableStateOf(formatDecimal(currentRating)) }
     var parsedDouble by rememberSaveable { mutableStateOf<Double?>(null) }
+
+    val focusManager = LocalFocusManager.current
+    DisposableEffect(Unit) {
+        onDispose { focusManager.clearFocus() }
+    }
 
     AlertDialog(
         onDismissRequest = onDismiss,
