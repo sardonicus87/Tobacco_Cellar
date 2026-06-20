9994374cdc60008e14eb8dc1e82c99358dc47ced
sardonicus87 <fx_forever@mail.com>
2026-06-20 17:57:52 -0500

    Refactor: - use a `BackupRestoreOption` enum instead of raw strings for state management - Implement `BackHandler` in `BackupDialog` and `RestoreDialog` to reset selection states and option (go back to original backup/restore selection instead of outright dismissing dialog) - Enhance UX by making checkbox labels clickable rather than just the checkboxes - Simplify implementations by removing redundant/unused modifiers and cleanup code

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/BackupRestoreDialog.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/BackupRestoreDialog.kt
@@ -1,5 +1,7 @@
 package com.sardonicus.tobaccocellar.ui.settings.appDatabaseDialogs
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +24,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,31 +40,26 @@ fun BackupRestoreDialog(
     onDismiss: () -> Unit,
     onSave: (String) -> Unit,
     onRestore: () -> Unit,
-    viewmodel: SettingsViewModel,
-    modifier: Modifier = Modifier
+    viewmodel: SettingsViewModel
 ) {
-    var option: String? by rememberSaveable { mutableStateOf(null) }
-    val updateOption: (String?) -> Unit = { option = it }
+    var option: BackupRestoreOption? by rememberSaveable { mutableStateOf(null) }
+    val updateOption: (BackupRestoreOption?) -> Unit = { option = it }
 
     val backupState by viewmodel.backupState.collectAsState()
     val restoreState by viewmodel.restoreState.collectAsState()
 
     AlertDialog(
         onDismissRequest = { onDismiss() },
-        modifier = modifier
-            .padding(0.dp),
         properties = DialogProperties(
             dismissOnBackPress = true,
             dismissOnClickOutside = true
         ),
         text = {
             when (option) {
-                "Backup" -> BackupDialog(backupState, viewmodel)
-                "Restore" -> RestoreDialog(restoreState, viewmodel)
+                BackupRestoreOption.BACKUP -> BackupDialog(backupState, viewmodel) { updateOption(null) }
+                BackupRestoreOption.RESTORE -> RestoreDialog(restoreState, viewmodel) { updateOption(null) }
                 null -> {
                     Column(
-                        modifier = Modifier
-                            .padding(bottom = 0.dp),
                         verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
                         Text(
@@ -69,19 +68,15 @@ fun BackupRestoreDialog(
                             color = LocalContentColor.current
                         )
                         Column(
-                            modifier = Modifier
-                                // .padding(start = 16.dp)
-                                .fillMaxWidth()
-                            ,
+                            modifier = Modifier.fillMaxWidth(),
                             verticalArrangement = Arrangement.spacedBy(6.dp),
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             Spacer(Modifier.height(8.dp))
                             TextButton(
-                                onClick = { updateOption("Backup") },
+                                onClick = { updateOption(BackupRestoreOption.BACKUP) },
                                 contentPadding = PaddingValues(8.dp, 3.dp),
-                                modifier = modifier
-                                    .heightIn(28.dp, 28.dp)
+                                modifier = Modifier.heightIn(28.dp, 28.dp)
                             ) {
                                 Text(
                                     text = "Backup",
@@ -89,10 +84,9 @@ fun BackupRestoreDialog(
                                 )
                             }
                             TextButton(
-                                onClick = { updateOption("Restore") },
+                                onClick = { updateOption(BackupRestoreOption.RESTORE) },
                                 contentPadding = PaddingValues(8.dp, 3.dp),
-                                modifier = modifier
-                                    .heightIn(28.dp, 28.dp)
+                                modifier = Modifier.heightIn(28.dp, 28.dp)
                             ) {
                                 Text(
                                     text = "Restore",
@@ -104,43 +98,26 @@ fun BackupRestoreDialog(
                 }
             }
         },
-        dismissButton = {
-            TextButton(
-                onClick = { onDismiss() },
-                modifier = Modifier
-                    .padding(0.dp)
-            ) {
-                Text(stringResource(R.string.cancel))
-            }
-        },
+        dismissButton = { TextButton({ onDismiss() }) { Text(stringResource(R.string.cancel)) } },
         confirmButton = {
             when (option) {
-                "Backup" -> {
+                BackupRestoreOption.BACKUP -> {
                     TextButton(
                         onClick = {
                             val suggestedFilename = backupState.suggestedFilename
                             onSave(suggestedFilename)
                         },
-                        modifier = Modifier
-                            .padding(0.dp),
                         enabled = backupState.databaseChecked || backupState.settingsChecked
-                    ) {
-                        Text(stringResource(R.string.save))
-                    }
+                    ) { Text(stringResource(R.string.save)) }
                 }
-                "Restore" -> {
+                BackupRestoreOption.RESTORE -> {
                     TextButton(
                         onClick = { onRestore() },
-                        modifier = Modifier
-                            .padding(0.dp),
                         enabled = restoreState.databaseChecked || restoreState.settingsChecked
-                    ) {
-                        Text(text = "Open")
-                    }
+                    ) { Text("Open") }
                 }
                 null -> { }
             }
-
         },
         containerColor = MaterialTheme.colorScheme.background,
         textContentColor = MaterialTheme.colorScheme.onBackground,
@@ -152,12 +129,14 @@ fun BackupRestoreDialog(
 @Composable
 private fun BackupDialog(
     backupState: BackupState,
-    viewmodel: SettingsViewModel,
-    modifier: Modifier = Modifier,
+    viewModel: SettingsViewModel,
+    resetState: () -> Unit,
 ) {
+    BackHandler {
+        viewModel.onBackupOptionChanged(backupState.copy(databaseChecked = false, settingsChecked = false))
+        resetState()
+    }
     Column (
-        modifier = modifier
-            .padding(bottom = 0.dp),
         verticalArrangement = Arrangement.spacedBy(0.dp)
     ) {
         Text(
@@ -165,32 +144,27 @@ private fun BackupDialog(
                     "be created that holds both. The restore function can optionally " +
                     "restore either the database or the settings from the joint backup " +
                     "file.",
-            modifier = Modifier
-                .padding(bottom = 12.dp),
+            modifier = Modifier.padding(bottom = 12.dp),
             fontSize = 15.sp,
             color = LocalContentColor.current
         )
         CheckboxWithLabel(
             text = "Database",
             checked = backupState.databaseChecked,
-            onCheckedChange = {
-                viewmodel.onBackupOptionChanged(
-                    backupState.copy(databaseChecked = it)
-                )
-            },
+            onCheckedChange = { },
             modifier = Modifier
-                .padding(bottom = 0.dp),
+                .clip(RoundedCornerShape(25))
+                .clickable { viewModel.onBackupOptionChanged(backupState.copy(databaseChecked = !backupState.databaseChecked)) }
+                .padding(bottom = 1.dp),
         )
         CheckboxWithLabel(
             text = "Settings",
             checked = backupState.settingsChecked,
-            onCheckedChange = {
-                viewmodel.onBackupOptionChanged(
-                    backupState.copy(settingsChecked = it)
-                )
-            },
+            onCheckedChange = { },
             modifier = Modifier
-                .padding(bottom = 0.dp),
+                .clip(RoundedCornerShape(25))
+                .clickable { viewModel.onBackupOptionChanged(backupState.copy(settingsChecked = !backupState.settingsChecked)) }
+                .padding(bottom = 1.dp),
         )
     }
 }
@@ -198,12 +172,14 @@ private fun BackupDialog(
 @Composable
 private fun RestoreDialog(
     restoreState: RestoreState,
-    viewmodel: SettingsViewModel,
-    modifier: Modifier = Modifier,
+    viewModel: SettingsViewModel,
+    resetState: () -> Unit,
 ) {
+    BackHandler {
+        viewModel.onRestoreOptionChanged(restoreState.copy(databaseChecked = false, settingsChecked = false))
+        resetState()
+    }
     Column (
-        modifier = modifier
-            .padding(bottom = 0.dp),
         verticalArrangement = Arrangement.spacedBy(0.dp)
     ) {
         Text(
@@ -211,31 +187,29 @@ private fun RestoreDialog(
                     "settings and/or database data (depending on which is selected). " +
                     "Either or both can be restored from a joint database/settings backup " +
                     "file.",
-            modifier = Modifier
-                .padding(bottom = 12.dp),
+            modifier = Modifier.padding(bottom = 12.dp),
             fontSize = 15.sp,
             color = LocalContentColor.current
         )
         CheckboxWithLabel(
             text = "Database",
             checked = restoreState.databaseChecked,
-            onCheckedChange = {
-                viewmodel.onRestoreOptionChanged(
-                    restoreState.copy(databaseChecked = it)
-                ) },
+            onCheckedChange = { },
             modifier = Modifier
-                .padding(bottom = 0.dp),
+                .clip(RoundedCornerShape(25))
+                .clickable { viewModel.onRestoreOptionChanged(restoreState.copy(databaseChecked = !restoreState.databaseChecked)) }
+                .padding(bottom = 1.dp),
         )
         CheckboxWithLabel(
             text = "Settings",
             checked = restoreState.settingsChecked,
-            onCheckedChange = {
-                viewmodel.onRestoreOptionChanged(
-                    restoreState.copy(settingsChecked = it)
-                )
-            },
+            onCheckedChange = { },
             modifier = Modifier
-                .padding(bottom = 0.dp),
+                .clip(RoundedCornerShape(25))
+                .clickable { viewModel.onRestoreOptionChanged(restoreState.copy(settingsChecked = !restoreState.settingsChecked)) }
+                .padding(bottom = 1.dp),
         )
     }
 }
+
+enum class BackupRestoreOption { BACKUP, RESTORE }
