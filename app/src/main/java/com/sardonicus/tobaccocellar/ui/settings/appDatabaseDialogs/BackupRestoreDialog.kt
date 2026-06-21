ee4643d453ac531b73d470bcb2db6664f9b6b636
sardonicus87 <fx_forever@mail.com>
2026-06-21 01:38:28 -0500

    Remove now obsolete clickable modifier since CheckboxWithLabel is now as a whole clickable internally

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/BackupRestoreDialog.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/BackupRestoreDialog.kt
@@ -1,7 +1,6 @@
 package com.sardonicus.tobaccocellar.ui.settings.appDatabaseDialogs
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -24,7 +22,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -151,20 +148,12 @@ private fun BackupDialog(
         CheckboxWithLabel(
             text = "Database",
             checked = backupState.databaseChecked,
-            onCheckedChange = { },
-            modifier = Modifier
-                .clip(RoundedCornerShape(25))
-                .clickable { viewModel.onBackupOptionChanged(backupState.copy(databaseChecked = !backupState.databaseChecked)) }
-                .padding(bottom = 1.dp),
+            onCheckedChange = { viewModel.onBackupOptionChanged(backupState.copy(databaseChecked = !backupState.databaseChecked)) }
         )
         CheckboxWithLabel(
             text = "Settings",
             checked = backupState.settingsChecked,
-            onCheckedChange = { },
-            modifier = Modifier
-                .clip(RoundedCornerShape(25))
-                .clickable { viewModel.onBackupOptionChanged(backupState.copy(settingsChecked = !backupState.settingsChecked)) }
-                .padding(bottom = 1.dp),
+            onCheckedChange = { viewModel.onBackupOptionChanged(backupState.copy(settingsChecked = !backupState.settingsChecked)) }
         )
     }
 }
@@ -194,20 +183,12 @@ private fun RestoreDialog(
         CheckboxWithLabel(
             text = "Database",
             checked = restoreState.databaseChecked,
-            onCheckedChange = { },
-            modifier = Modifier
-                .clip(RoundedCornerShape(25))
-                .clickable { viewModel.onRestoreOptionChanged(restoreState.copy(databaseChecked = !restoreState.databaseChecked)) }
-                .padding(bottom = 1.dp),
+            onCheckedChange = { viewModel.onRestoreOptionChanged(restoreState.copy(databaseChecked = !restoreState.databaseChecked)) }
         )
         CheckboxWithLabel(
             text = "Settings",
             checked = restoreState.settingsChecked,
-            onCheckedChange = { },
-            modifier = Modifier
-                .clip(RoundedCornerShape(25))
-                .clickable { viewModel.onRestoreOptionChanged(restoreState.copy(settingsChecked = !restoreState.settingsChecked)) }
-                .padding(bottom = 1.dp),
+            onCheckedChange = { viewModel.onRestoreOptionChanged(restoreState.copy(settingsChecked = !restoreState.settingsChecked)) }
         )
     }
 }
