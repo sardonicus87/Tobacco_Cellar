aae2160daa842f351588b7642bc2a7812663e2ed
sardonicus87 <fx_forever@mail.com>
2026-07-29 03:47:20 -0500

    - Add `extraBufferCapacity` to `EventBus` shared flow to prevent emitter suspension - Implement `tryEmit` function for non-suspending event emission - Define `ShowSnackbar` and `DismissSnackbar` event types for global UI orchestration

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/utilities/EventBus.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/utilities/EventBus.kt
@@ -4,10 +4,17 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 object EventBus {
-    private val _events = MutableSharedFlow<Any>()
+    private val _events = MutableSharedFlow<Any>(extraBufferCapacity = 64)
     val events = _events.asSharedFlow()
 
     suspend fun emit(event: Any) {
         _events.emit(event)
     }
+
+    fun tryEmit(event: Any) {
+        _events.tryEmit(event)
+    }
 }
+
+data class ShowSnackbar (val message: String)
+data object DismissSnackbar
