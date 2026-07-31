1f8dafae2fbf27911b014778b1f5a01ad624e7dc
sardonicus87 <fx_forever@mail.com>
2026-07-31 03:19:09 -0500

    - Added loading state with 50ms debounce to show loading indicator on dialog and disable buttons if updateTinSync() isn't near instant - delay automatic dismissal until loading state is finished (with 25ms debounce to allow potential loading state to be detected when clicking save)

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/TinRatesDialog.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/settings/appDatabaseDialogs/TinRatesDialog.kt
@@ -2,6 +2,7 @@ package com.sardonicus.tobaccocellar.ui.settings.appDatabaseDialogs
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -19,13 +20,16 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
@@ -39,13 +43,18 @@ import androidx.compose.ui.window.DialogProperties
 import com.sardonicus.tobaccocellar.R
 import com.sardonicus.tobaccocellar.ui.blendDetails.formatDecimal
 import com.sardonicus.tobaccocellar.ui.composables.CustomTextField
+import com.sardonicus.tobaccocellar.ui.composables.LoadingIndicator
 import com.sardonicus.tobaccocellar.ui.theme.LocalCustomColors
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import java.text.DecimalFormatSymbols
 import java.util.Locale
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 fun TinRatesDialog(
     onDismiss: () -> Unit,
+    loading: Boolean,
     ozRate: Double,
     gramsRate: Double,
     onSave: (Double, Double) -> Unit
@@ -53,6 +62,28 @@ fun TinRatesDialog(
     var tinOzRate by rememberSaveable { mutableStateOf(formatDecimal(ozRate)) }
     var tinGramsRate by rememberSaveable { mutableStateOf(formatDecimal(gramsRate)) }
     val focusManager = LocalFocusManager.current
+    var saving by rememberSaveable { mutableStateOf(false) }
+    var debouncedLoading by remember { mutableStateOf(false) }
+
+    LaunchedEffect(loading) {
+        if (loading) {
+            delay(50.milliseconds)
+            debouncedLoading = true
+        }
+    }
+
+    LaunchedEffect(saving, loading) {
+        if (saving && !loading) {
+            focusManager.clearFocus()
+            delay(25.milliseconds)
+            onDismiss()
+        }
+        else if (saving) {
+            focusManager.clearFocus()
+            snapshotFlow { loading }.first { !it }
+            onDismiss()
+        }
+    }
 
     DisposableEffect(Unit) { onDispose { focusManager.clearFocus() } }
 
@@ -63,130 +94,155 @@ fun TinRatesDialog(
             dismissOnClickOutside = true
         ),
         text = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(MaterialTheme.colorScheme.background)
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 16.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.Start
-            ) {
-                Row(
+            Box {
+                Column(
                     modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .offset(x = (-8).dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 16.dp)
+                        .alpha(if (debouncedLoading) .38f else 1f),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.Start
                 ) {
-                    Text(
-                        text = "One Tin = ",
-                        modifier = Modifier.padding(end = 8.dp),
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 16.sp,
-                    )
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterVertically),
-                        horizontalAlignment = Alignment.Start
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .offset(x = (-8).dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
                     ) {
-                        val symbols = remember { DecimalFormatSymbols.getInstance(Locale.getDefault()) }
-                        val allowedPattern = remember(symbols.decimalSeparator, symbols.groupingSeparator) {
-                            val ds = Regex.escape(symbols.decimalSeparator.toString())
-                            Regex("^(\\s*|\\d{0,5}($ds\\d{0,2})?)$")
-                        }
+                        Text(
+                            text = "One Tin = ",
+                            modifier = Modifier.padding(end = 8.dp),
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 16.sp
+                        )
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(
+                                2.dp,
+                                Alignment.CenterVertically
+                            ),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            val symbols =
+                                remember { DecimalFormatSymbols.getInstance(Locale.getDefault()) }
+                            val allowedPattern =
+                                remember(symbols.decimalSeparator, symbols.groupingSeparator) {
+                                    val ds = Regex.escape(symbols.decimalSeparator.toString())
+                                    Regex("^(\\s*|\\d{0,5}($ds\\d{0,2})?)$")
+                                }
 
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
-                        ) {
-                            CustomTextField(
-                                value = tinOzRate,
-                                onValueChange = { if (it.matches(allowedPattern)) { tinOzRate = it } },
-                                modifier = Modifier.width(80.dp),
-                                textStyle = LocalTextStyle.current.copy(
-                                    textAlign = TextAlign.End,
-                                    color = LocalContentColor.current,
-                                ),
-                                keyboardOptions = KeyboardOptions(
-                                    keyboardType = KeyboardType.Number,
-                                    imeAction = ImeAction.Done
-                                ),
-                                shape = MaterialTheme.shapes.extraSmall,
-                                colors = TextFieldDefaults.colors(
-                                    focusedIndicatorColor = Color.Transparent,
-                                    unfocusedIndicatorColor = Color.Transparent,
-                                    disabledIndicatorColor = Color.Transparent,
-                                    focusedContainerColor = LocalCustomColors.current.textField,
-                                    unfocusedContainerColor = LocalCustomColors.current.textField,
-                                    disabledContainerColor = LocalCustomColors.current.textField,
-                                    cursorColor = MaterialTheme.colorScheme.primary,
-                                ),
-                                minLines = 1,
-                                contentPadding = PaddingValues(vertical = 6.dp, horizontal = 12.dp),
-                                placeholder = {
-                                    Text(
-                                        text = "(${formatDecimal(ozRate)})",
-                                        modifier = Modifier.fillMaxWidth(),
-                                        style = LocalTextStyle.current.copy(
-                                            textAlign = TextAlign.End,
-                                            fontSize = 13.5.sp,
-                                            lineHeight = 20.sp,
-                                            color = LocalContentColor.current.copy(alpha = .38f),
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
+                            ) {
+                                CustomTextField(
+                                    value = tinOzRate,
+                                    onValueChange = {
+                                        if (it.matches(allowedPattern)) {
+                                            tinOzRate = it
+                                        }
+                                    },
+                                    modifier = Modifier.width(80.dp),
+                                    textStyle = LocalTextStyle.current.copy(
+                                        textAlign = TextAlign.End,
+                                        color = LocalContentColor.current,
+                                    ),
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Number,
+                                        imeAction = ImeAction.Done
+                                    ),
+                                    shape = MaterialTheme.shapes.extraSmall,
+                                    colors = TextFieldDefaults.colors(
+                                        focusedIndicatorColor = Color.Transparent,
+                                        unfocusedIndicatorColor = Color.Transparent,
+                                        disabledIndicatorColor = Color.Transparent,
+                                        focusedContainerColor = LocalCustomColors.current.textField,
+                                        unfocusedContainerColor = LocalCustomColors.current.textField,
+                                        disabledContainerColor = LocalCustomColors.current.textField,
+                                        cursorColor = MaterialTheme.colorScheme.primary,
+                                    ),
+                                    enabled = !debouncedLoading,
+                                    maxLines = 1,
+                                    contentPadding = PaddingValues(
+                                        vertical = 6.dp,
+                                        horizontal = 12.dp
+                                    ),
+                                    placeholder = {
+                                        Text(
+                                            text = "(${formatDecimal(ozRate)})",
+                                            modifier = Modifier.fillMaxWidth(),
+                                            style = LocalTextStyle.current.copy(
+                                                textAlign = TextAlign.End,
+                                                fontSize = 13.5.sp,
+                                                lineHeight = 20.sp,
+                                                color = LocalContentColor.current.copy(alpha = .38f),
+                                            )
                                         )
-                                    )
-                                }
-                            )
-                            Text("oz")
-                        }
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
-                        ) {
-                            CustomTextField(
-                                value = tinGramsRate,
-                                onValueChange = { if (it.matches(allowedPattern)) { tinGramsRate = it } },
-                                modifier = Modifier
-                                    .width(80.dp),
-                                textStyle = LocalTextStyle.current.copy(
-                                    textAlign = TextAlign.End,
-                                    color = LocalContentColor.current,
-                                ),
-                                keyboardOptions = KeyboardOptions(
-                                    keyboardType = KeyboardType.Number,
-                                    imeAction = ImeAction.Done
-                                ),
-                                shape = MaterialTheme.shapes.extraSmall,
-                                colors = TextFieldDefaults.colors(
-                                    focusedIndicatorColor = Color.Transparent,
-                                    unfocusedIndicatorColor = Color.Transparent,
-                                    disabledIndicatorColor = Color.Transparent,
-                                    focusedContainerColor = LocalCustomColors.current.textField,
-                                    unfocusedContainerColor = LocalCustomColors.current.textField,
-                                    disabledContainerColor = LocalCustomColors.current.textField,
-                                    cursorColor = MaterialTheme.colorScheme.primary,
-                                ),
-                                maxLines = 1,
-                                contentPadding = PaddingValues(vertical = 6.dp, horizontal = 12.dp),
-                                placeholder = {
-                                    Text(
-                                        text = "(${formatDecimal(gramsRate)})",
-                                        modifier = Modifier.fillMaxWidth(),
-                                        style = LocalTextStyle.current.copy(
-                                            textAlign = TextAlign.End,
-                                            fontSize = 13.5.sp,
-                                            lineHeight = 20.sp,
-                                            color = LocalContentColor.current.copy(alpha = .38f),
+                                    }
+                                )
+                                Text("oz")
+                            }
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
+                            ) {
+                                CustomTextField(
+                                    value = tinGramsRate,
+                                    onValueChange = {
+                                        if (it.matches(allowedPattern)) {
+                                            tinGramsRate = it
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .width(80.dp),
+                                    textStyle = LocalTextStyle.current.copy(
+                                        textAlign = TextAlign.End,
+                                        color = LocalContentColor.current,
+                                    ),
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Number,
+                                        imeAction = ImeAction.Done
+                                    ),
+                                    shape = MaterialTheme.shapes.extraSmall,
+                                    colors = TextFieldDefaults.colors(
+                                        focusedIndicatorColor = Color.Transparent,
+                                        unfocusedIndicatorColor = Color.Transparent,
+                                        disabledIndicatorColor = Color.Transparent,
+                                        focusedContainerColor = LocalCustomColors.current.textField,
+                                        unfocusedContainerColor = LocalCustomColors.current.textField,
+                                        disabledContainerColor = LocalCustomColors.current.textField,
+                                        cursorColor = MaterialTheme.colorScheme.primary,
+                                    ),
+                                    enabled = !debouncedLoading,
+                                    maxLines = 1,
+                                    contentPadding = PaddingValues(
+                                        vertical = 6.dp,
+                                        horizontal = 12.dp
+                                    ),
+                                    placeholder = {
+                                        Text(
+                                            text = "(${formatDecimal(gramsRate)})",
+                                            modifier = Modifier.fillMaxWidth(),
+                                            style = LocalTextStyle.current.copy(
+                                                textAlign = TextAlign.End,
+                                                fontSize = 13.5.sp,
+                                                lineHeight = 20.sp,
+                                                color = LocalContentColor.current.copy(alpha = .38f),
+                                            )
                                         )
-                                    )
-                                }
-                            )
-                            Text("grams")
+                                    }
+                                )
+                                Text("grams")
+                            }
                         }
                     }
                 }
+                if (debouncedLoading) { LoadingIndicator(center = true, modifier = Modifier.matchParentSize()) }
             }
         },
-        dismissButton = { TextButton({ onDismiss() }) { Text(stringResource(R.string.cancel)) } },
+        dismissButton = { TextButton({ onDismiss() }, enabled = !debouncedLoading) { Text(stringResource(R.string.cancel)) } },
         confirmButton = {
             TextButton(
                 onClick = {
@@ -194,8 +250,9 @@ fun TinRatesDialog(
                         tinOzRate.toDoubleOrNull() ?: ozRate,
                         tinGramsRate.toDoubleOrNull() ?: gramsRate
                     )
-                    onDismiss()
-                }
+                    saving = true
+                },
+                enabled = !debouncedLoading
             ) { Text(stringResource(R.string.save)) }
         },
         containerColor = MaterialTheme.colorScheme.background,
