468ef87969d7ae5ff8d27dbfbf90da4f8969f5ae
sardonicus87 <fx_forever@mail.com>
2026-08-13 17:18:05 -0500

    Refactor font certificate resources: - Rename `com_google_android_gms_fonts_certs` to `fonts_certs` in `font_certs.xml` - Remove redundant `com_google_android_gms_fonts_certs_dev` and `com_google_android_gms_fonts_certs_prod` string-array definitions - Update `GoogleFont.Provider` in `Type.kt` to reference the renamed `fonts_certs` array resource

--- app/src/main/java/com/sardonicus/tobaccocellar/ui/theme/Type.kt
+++ app/src/main/java/com/sardonicus/tobaccocellar/ui/theme/Type.kt
@@ -9,7 +9,7 @@ import com.sardonicus.tobaccocellar.R
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
+    certificates = R.array.fonts_certs
 )
 
 val bodyFontFamily = FontFamily(
